dda0391f73d9e2e18326e025ad42a7a1c01c36d5
hfghfg123456 <zingme204@gmail.com>
2023-11-21 20:47:25 +0700

    da co purchase

--- src/frontend/components/cart.js
+++ src/frontend/components/cart.js
@@ -1,7 +1,34 @@
 import React, { useState } from "react"
 import './cart.css'
+import { ethers } from 'ethers';
 export default function Cart({ cart, setCart, setCartCount }) {
 
+    
+
+    async function purchase() {
+        // Request account access
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+        // Create a signer object
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+
+        // Calculate total price of cart items
+        const totalPrice = cart.reduce((total, item) => total + parseFloat(item.price.replace(/[^\d.-]/g, '')), 0);
+
+        // Convert total price to wei (1 ether = 10^18 wei)
+        const totalPriceInWei = ethers.utils.parseEther(totalPrice.toString());
+
+        // Send transaction
+        const tx = await signer.sendTransaction({
+            to: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8', // Replace with the merchant's address
+            value: totalPriceInWei,
+        });
+
+        console.log(`Transaction sent with hash: ${tx.hash}`);
+    }
+      
+    
     const deleteItem = (title) => {
         console.log(title);
         setCart(prev => prev.filter(item => item.title !== title))
@@ -33,7 +60,8 @@ export default function Cart({ cart, setCart, setCartCount }) {
                     ))}
                 </tbody>
             </table>
-            <button className="checkout-btn">Purchase</button>
+            <button className="checkout-btn" onClick={purchase}>Purchase</button>
         </div>
+        
     )
 }
